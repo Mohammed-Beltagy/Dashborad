55f2e3d0eebbb118c5825bc98fa7e215d0410741
Mohammed-Beltagy <justbeltagy@gmail.com>
2023-03-20 13:52:03 +0200

    Dashboard Finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,56 @@
 const gulp = require("gulp"),
-      connect = require('gulp-connect'),
-      pug = require("gulp-pug"),
-      sass = require('gulp-sass')(require('sass')),
-      autoprefixer = require('gulp-autoprefixer'),
-      sourcemaps = require('gulp-sourcemaps'),
-      uglify = require('gulp-uglify');
+  connect = require("gulp-connect"),
+  pug = require("gulp-pug"),
+  sass = require("gulp-sass")(require("sass")),
+  autoprefixer = require("gulp-autoprefixer"),
+  sourcemaps = require("gulp-sourcemaps"),
+  uglify = require("gulp-uglify");
 
 // Compile Pug Files
-gulp.task('compile-pug', () => gulp.src('./stage/html/*.pug')
-                                    .pipe(pug())
-                                    .pipe(gulp.dest("./docs/"))
-                                    .pipe(connect.reload())
-)
+gulp.task("compile-pug", () =>
+  gulp
+    .src("./stage/html/*.pug")
+    .pipe(pug())
+    .pipe(gulp.dest("./docs/"))
+    .pipe(connect.reload())
+);
 
 // Compile Scss Files With Prefixes
-gulp.task('compile-sass', () => gulp.src('./stage/sass/main.scss')
-                                    .pipe(sourcemaps.init())
-                                    .pipe(sass({outputStyle: "compressed"}))
-                                    .pipe(autoprefixer("last 2 versions"))
-                                    .pipe(sourcemaps.write())
-                                    .pipe(gulp.dest("./docs/assets/"))
-                                    .pipe(connect.reload())
-)
+gulp.task("compile-sass", () =>
+  gulp
+    .src("./stage/sass/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(autoprefixer("last 2 versions"))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./docs/assets/"))
+    .pipe(connect.reload())
+);
 
 // Redirect JS Files
-gulp.task('redirect-js', () => gulp.src('./stage/js/app.js')
-                                    .pipe(uglify())
-                                    .pipe(gulp.dest('./docs/assets/'))
-                                    .pipe(connect.reload())
-)
+gulp.task("redirect-js", () =>
+  gulp
+    .src("./stage/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./docs/assets/"))
+    .pipe(connect.reload())
+);
 
-// Redirect Images 
-gulp.task('redirect-images', () => gulp.src('./stage/images/**/*.*').pipe(gulp.dest('./docs/assets/images/')))
+// Redirect Images
+gulp.task("redirect-images", () =>
+  gulp.src("./stage/images/**/*.*").pipe(gulp.dest("./docs/assets/images/"))
+);
 
 // Start Server & Watch Changes
-gulp.task('default', () => {
+gulp.task("default", () => {
   connect.server({
-    root: './docs/',
-    livereload: true
+    root: "./docs/",
+    livereload: true,
   });
-  gulp.watch('./stage/html/**/*.pug', gulp.series(['compile-pug', 'redirect-images']))
-  gulp.watch('./stage/sass/**/*.scss', gulp.series('compile-sass'))
-  gulp.watch('./stage/js/**/*.js', gulp.series('redirect-js'))
-})
+  gulp.watch(
+    "./stage/html/**/*.pug",
+    gulp.series(["compile-pug", "redirect-images"])
+  );
+  gulp.watch("./stage/sass/**/*.scss", gulp.series("compile-sass"));
+  gulp.watch("./stage/js/**/*.js", gulp.series("redirect-js"));
+});
